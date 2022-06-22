f98b212d55e63486d7d8901b20c0c1767053f2b1
Claudeci Siqueira de Lucena Júnior <cast.claudeci.utic@sebrae.com.br>
2022-06-22 08:34:33 -0300

    Projeto01/Aprimorando a aplicação/TypeScript no React

---
+++ src/components/Comment.tsx
@@ -0,0 +1,53 @@
+import style from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment () {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return (
+        <div className={style.comment}>
+            <Avatar hasBorder={false} src='https://github.com/claudecijr77.png' alt="" />
+
+            <div className={style.commentBox}>
+                <div className={style.commentContent}>
+                    <header>
+                        <div className={style.authorAndTime}>
+                            <strong>Claudeci Júnior</strong>
+                            <time title='quinze de junho às 05:14' dateTime='2022-06-15 05:14'>
+                                Cerca de 2h atrás
+                            </time>
+                        </div>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{ content }</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
